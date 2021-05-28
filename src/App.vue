d50fc4ce5dede9ef6066b42cccfd295aeaca9d17
William <william@trezlabs.com>
2021-05-27 21:26:12 -0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,105 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <CustomDropdown
+      :items="states"
+      placeholder="foobar"
+      multiSelect
+      searchable
+      isChecked
+      @elementsSelected="selectedFilters($event)"
+      @hidden="close"
+    >
+      <template #btnText>
+        show options &#9729;
+      </template>
+    </CustomDropdown>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import CustomDropdown from "./components/CustomDropdown.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      states: [
+        {
+          id: "CO",
+          value: "CO1",
+          name: "East South Central"
+        },
+        {
+          id: "CO",
+          value: "CO2",
+          name: "West South Central"
+        },
+        {
+          id: "CO",
+          value: "CO3",
+          name: "New England"
+        },
+        {
+          id: "CO",
+          value: "CO4",
+          name: "South Atlantic"
+        },
+        {
+          id: "CO",
+          value: "CO5",
+          name: "East North Central"
+        },
+        {
+          id: "CO",
+          value: "CO6",
+          name: "Mid Atlantic"
+        },
+        {
+          id: "CO",
+          value: "CO7",
+          name: "Alabama"
+        },
+        {
+          id: "CO",
+          value: "CO8",
+          name: "Arizona"
+        },
+        {
+          id: "CO",
+          value: "CO9",
+          name: "Arkansas"
+        },
+        {
+          id: "CO",
+          value: "CO10",
+          name: "California",
+          disabled: true
+        },
+        {
+          id: "CO",
+          value: "CO11",
+          name: "Colorado",
+          checked: true
+        }
+      ]
+    };
+  },
   components: {
-    HelloWorld
+    CustomDropdown
+    // HelloWorld
+  },
+  methods: {
+    selectedFilters(e) {
+      console.log(e);
+    },
+    close() {
+      console.log("close");
+    }
   }
-}
+};
 </script>
 
 <style>
---
+++ src/components/CustomDropdown.vue
@@ -0,0 +1,255 @@
+<template>
+  <div class="" v-clickoutside="onClose">
+    <!-- button -->
+    <button class="btn-main-class" @click="saveLocations">
+      <slot v-if="hasBtnText" name="btnText"></slot>
+      <template v-else>{{ placeholder }}</template>
+    </button>
+
+    <!-- select body -->
+    <div v-if="popupLocationFilter" class="bg">
+      <!-- search input -->
+      <div class="w-full" v-if="searchable">
+        <input
+          v-model="stateToSearch"
+          class=""
+          :placeholder="searchPlaceholder"
+          @input="handleTyping()"
+        />
+      </div>
+
+      <!-- options container -->
+      <div class="">
+        <div
+          :key="index"
+          v-for="(item, index) in elements"
+          class="pointer"
+          :class="{ hide: !item.matches }"
+        >
+          <p
+            class="flex"
+            v-if="isChecked"
+            :class="{
+              selected: item.checked && !item.disabled,
+              disabled: item.disabled
+            }"
+            @click="handleSelection(item)"
+          >
+            <input
+              class="pointer"
+              type="checkbox"
+              :id="item.name"
+              :name="item.name"
+              :value="item.name"
+              :checked="item.checked && !item.disabled"
+              :disabled="item.disabled"
+            />
+            <label
+              @click="handleSelection(item)"
+              :for="item.name"
+              class="pointer"
+              >{{ item.name }}</label
+            >
+          </p>
+          <div v-else>
+            <p
+              @click="handleSelection(item)"
+              :for="item.name"
+              class="pointer"
+              :class="{ selected: item.checked }"
+            >
+              {{ item.name }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DropdownCustomize",
+  props: {
+    items: {
+      type: Array,
+      default: () => []
+    },
+    multiSelect: {
+      type: Boolean,
+      default: false
+    },
+    searchable: {
+      type: Boolean,
+      default: false
+    },
+    isChecked: {
+      type: Boolean,
+      default: false
+    },
+    placeholder: {
+      type: String,
+      default: "Select an Option"
+    },
+    searchPlaceholder: {
+      type: String,
+      default: ""
+    }
+  },
+  data() {
+    return {
+      stateToSearch: "",
+      popupLocationFilter: false,
+      selectedToExport: [],
+      elements: []
+    };
+  },
+  computed: {
+    hasBtnText() {
+      return !!this.$slots.btnText;
+    }
+  },
+  created() {
+    this.formatItems();
+  },
+  methods: {
+    onClose() {
+      this.saveLocations();
+      this.popupLocationFilter = false;
+    },
+    clearFilters() {
+      this.elements.forEach((item) => {
+        item.checked = false;
+        item.matches = true;
+      });
+
+      this.stateToSearch = "";
+    },
+    handleTyping() {
+      this.elements.forEach((item) => {
+        let matchWithStateName = item.name
+          .toLowerCase()
+          .includes(this.stateToSearch.toLowerCase());
+
+        item.matches = matchWithStateName ? true : false;
+      });
+    },
+    formatItems() {
+      this.elements = this.items;
+      this.elements.forEach((item) => {
+        item.matches = true;
+        if (item.checked) {
+          this.selectedToExport.push(item.value);
+        } else {
+          item.checked = false;
+        }
+        if (!item.disabled) {
+          item.disabled = false;
+        }
+      });
+    },
+    handleSelection(item) {
+      if (item.checked) {
+        if (!item.disabled) {
+          console.log("checked");
+          console.log(this.selectedToExport);
+
+          item.checked = false;
+          if (Array.isArray(this.selectedToExport)) {
+            this.selectedToExport = this.selectedToExport.filter(
+              (e) => e != item.value
+            );
+          }
+        }
+      } else {
+        console.log("not checked");
+        if (this.multiSelect) {
+          console.log("multi");
+          if (!item.disabled) {
+            this.selectedToExport.push(item.value);
+          }
+        } else {
+          console.log("not multi");
+          if (!item.disabled) {
+            this.selectedToExport = item.value;
+            this.elements.map((i) => {
+              i.checked = false;
+            });
+            // item.checked = false;
+          }
+          item.checked = true;
+        }
+      }
+      this.$forceUpdate();
+    },
+    saveLocations() {
+      this.popupLocationFilter = !this.popupLocationFilter;
+      this.$emit("elementsSelected", this.selectedToExport);
+      if (!this.popupLocationFilter) {
+        this.$emit("hidden");
+      }
+    },
+    cleanSelected(name) {
+      this.selectedToExport = this.selectedToExport.filter((e) => e != name);
+      name = name.split(" (")[0];
+    }
+  },
+  directives: {
+    clickoutside: {
+      bind: function(el, binding, node) {
+        el.clickOutsideEvent = function(event) {
+          if (!(el == event.target || el.contains(event.target))) {
+            node.context[binding.expression](event);
+          }
+        };
+        document.body.addEventListener("click", el.clickOutsideEvent);
+      },
+      unbind: function(el) {
+        document.body.removeEventListener("click", el.clickOutsideEvent);
+      },
+      stopProp(event) {
+        event.stopPropagation();
+      }
+    }
+  }
+};
+</script>
+
+<style lang="css" scoped>
+.btn-main-class {
+  position: relative;
+}
+.bg {
+  position: absolute;
+  border: 1px solid #ccc;
+  min-width: 25%;
+  z-index: 1000;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-shadow: 0 6px 12px rgb(0 0 0 / 18%);
+  background-clip: padding-box;
+}
+.flex {
+  display: flex;
+}
+.hide {
+  display: none !important;
+}
+.pointer {
+  cursor: pointer;
+}
+.selected {
+  background: #41b883;
+}
+.disabled {
+  color: #ccc;
+}
+p {
+  margin: 0;
+  padding: 0.5em;
+}
+label {
+  margin-left: 0.75em;
+}
+</style>
